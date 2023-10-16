f095e8b229fa2b058d22bdfc0be8d2d861294f4a
Sanjeet Kumar <sanjeetbth7@gmail.com>
2023-10-16 09:56:12 +0530

    Database Conected

---
+++ app.js
@@ -0,0 +1,145 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import ejs from "ejs";
+import dotenv from "dotenv";
+import mysql from "mysql2"
+
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+// Now you can use __dirname in your code
+
+
+dotenv.config();
+const PORT = 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('strict routing', true);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public",{index : " "}));  // When public folder have index.html then in dfault route instead of app.get("/"), It show index.html.  To avoid this i have added {index : " "}
+
+
+
+// Create the connection pool. The pool-specific settings are the defaults
+// instead of creating connection, here creating pool of connections, so that for each query , did not need to stablish connection
+const pool = mysql.createPool({
+    host: process.env.dbHOST,
+    user: process.env.dbUSER,
+    password: process.env.dbPASSWORD,
+    database: process.env.dbNAME
+}).promise(); // here using promise so we can use async ,instead of clasic callback
+
+
+
+
+
+
+app.get("/", async (req, res) => {
+    try {
+        res.sendFile(__dirname+"/index.html");
+        
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    } 
+});
+
+  
+app.get("/admin-dashboard", async (req, res) => {
+    try {
+        const Employees = await pool.query("SELECT * FROM Employee");
+        res.render("admin-dashboard",{employees : Employees[0]});
+        
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    } 
+});
+
+
+
+app.get("/login", async(req,res)=>{
+    try {
+        res.render("login");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get("/logout", (req,res)=>{
+  res.redirect("/login");
+})
+
+app.post("/admin-dashboard", async (req, res) => {
+    try {
+        const Employees = await pool.query("SELECT * FROM Employee");
+        res.render("admin-dashboard",{employees : Employees[0]});
+        
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    } 
+});
+
+
+
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+      
+    // Fetch user from the database
+    const results = await pool.query('SELECT * FROM Employee WHERE email =?', [username]);
+      
+    // Check if user with provided username exists
+    if (results.length > 0) {
+      const [user] = results[0];
+      if (password === user.password) {
+        res.render("dashboard",{user:user});
+      } 
+      else {
+        res.send('Incorrect password');
+      }
+    } 
+    else {
+      res.send('User not found');
+    }
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+  
+app.post("/logout", (req,res)=>{
+  res.redirect("/login");
+}) 
+
+
+
+
+
+
+
+
+
+
+// 404 error handling
+app.use((req, res, next) => {
+    res.status(404).render("404");
+});
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server started on port ${PORT}`);
+});
